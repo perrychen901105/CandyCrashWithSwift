868aaf55b8a0558e30f7d34fa96ac704a0b18283
perrychen901105 <chenzhipeng901105@gmail.com>
2014-06-27 18:37:28 +0800

    add images

---
+++ CookieCrunch/CookieCrunch/Cookie.swift
@@ -0,0 +1,44 @@
+//
+//  Cookie.swift
+//  CookieCrunch
+//
+//  Created by Perry on 14-6-27.
+//  Copyright (c) 2014年 perry. All rights reserved.
+//
+
+import SpriteKit
+
+enum CookieType: Int {
+    case Unknown = 0, Croissant, Cupcake, Danish, Donut, Macaroon, SugarCookie
+    var spriteName: String {
+        let spriteNames = [
+            "Croissant",
+            "Cupcake",
+            "Danish",
+            "Donut",
+            "Macaroon",
+            "SugarCookie"
+        ]
+        return spriteNames[toRaw() - 1]
+    }
+    
+    var highlightedSpriteName: String {
+        let highlightedSpriteNames = [
+            
+        ]
+    }
+}
+
+class Cookie {
+    var column: Int
+    var row: Int
+    let cookieType: CookieType
+    var sprite: SKSpriteNode?
+    
+    init(column: Int, row: Int, cookieType: CookieType) {
+        self.column = column
+        self.row = row
+        self.cookieType = cookieType
+    }
+}
+
--- CookieCrunch/CookieCrunch/GameViewController.swift
+++ CookieCrunch/CookieCrunch/GameViewController.swift
@@ -9,57 +9,30 @@
 import UIKit
 import SpriteKit
 
-extension SKNode {
-    class func unarchiveFromFile(file : NSString) -> SKNode? {
-        
-        let path = NSBundle.mainBundle().pathForResource(file, ofType: "sks")
-        
-        var sceneData = NSData.dataWithContentsOfFile(path, options: .DataReadingMappedIfSafe, error: nil)
-        var archiver = NSKeyedUnarchiver(forReadingWithData: sceneData)
-        
-        archiver.setClass(self.classForKeyedUnarchiver(), forClassName: "SKScene")
-        let scene = archiver.decodeObjectForKey(NSKeyedArchiveRootObjectKey) as GameScene
-        archiver.finishDecoding()
-        return scene
-    }
-}
-
 class GameViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        if let scene = GameScene.unarchiveFromFile("GameScene") as? GameScene {
-            // Configure the view.
-            let skView = self.view as SKView
-            skView.showsFPS = true
-            skView.showsNodeCount = true
-            
-            /* Sprite Kit applies additional optimizations to improve rendering performance */
-            skView.ignoresSiblingOrder = true
-            
-            /* Set the scale mode to scale to fit the window */
-            scene.scaleMode = .AspectFill
-            
-            skView.presentScene(scene)
-        }
+    var scene: GameScene!
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
     }
-
+    
     override func shouldAutorotate() -> Bool {
         return true
     }
-
-    override func supportedInterfaceOrientations() -> Int {
-        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
-            return Int(UIInterfaceOrientationMask.AllButUpsideDown.toRaw())
-        } else {
-            return Int(UIInterfaceOrientationMask.All.toRaw())
-        }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // configure the view
+        let skView = view as SKView
+        skView.multipleTouchEnabled = false
+        
+        // create and configue the scene.
+        scene = GameScene(size: skView.bounds.size)
+        scene.scaleMode = .AspectFill
+        
+        // present the scene
+        skView.presentScene(scene)
+    }
 }
